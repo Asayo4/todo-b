fb7d81e45adafb5ab3afa5daa1fa67b9476443c2
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-07 13:00:13 +0900

    renew

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\TodoController;
 
 
 Route::apiResource('/todo', TodoController::class);
+Route::post('/', [TodoController::class]);
+Route::get('/', [TodoController::class]);
+Route::put('/', [TodoController::class]);
+Route::delete('/', [TodoController::class]);
