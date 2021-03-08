7c3d3b21019c0bd946a7a52c1df8be975448192f
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-08 15:24:41 +0900

    repair

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TodoController;
 
 
 Route::apiResource('/todo', TodoController::class);
-Route::post('/todo', [TodoController::class]);
-Route::get('/todo', [TodoController::class]);
-Route::put('/todo', [TodoController::class]);
-Route::delete('/todo', [TodoController::class]);
+Route::post('/todo', [TodoController::class, 'post']);
+Route::get('/todo', [TodoController::class, 'get']);
+Route::put('/todo', [TodoController::class, 'put']);
+Route::delete('/todo', [TodoController::class, 'delete']);
