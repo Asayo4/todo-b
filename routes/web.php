0e38dfc199d98aa3780c14492208f20d5f1f424f
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-10 01:39:49 +0900

    good

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::delete("/aoi/todo/(id)", [TodoController::class, 'delete']);
+Route::put("/aoi/todo/(id)", [TodoController::class, 'put']);
