e950551af8049e47d1e3af630693562d264c735d
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-09 19:50:51 +0900

    できますように

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\TodoController;
 
 
 Route::apiResource('/todo', TodoController::class);
+Route::get('/todo', [TodoController::class, 'get']);
+Route::put('/todo', [TodoController::class, 'put']);
+Route::post('/todo', [TodoController::class, 'post']);
+Route::delete('/todo', [TodoController::class, 'delete']);
