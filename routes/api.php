a9281fea12bbfc1189d75c1ee349fda7436606e4
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-10 15:10:10 +0900

    message

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 
-Route::apiResource('/todos/{todo:id}', TodoController::class);
+Route::apiResource('/todos/{$id}', TodoController::class);
