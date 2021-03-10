6fc677b434430c547865f1a510b6923b70c29b18
Asayo4 <zilchctese@yahoo.co.jp>
2021-03-10 14:46:22 +0900

    message

--- database/migrations/2021_03_05_042604_create_todos_table.php
+++ database/migrations/2021_03_05_042604_create_todos_table.php
@@ -14,7 +14,7 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('todo');
         });
     }
